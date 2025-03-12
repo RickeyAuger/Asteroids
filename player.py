82660d57c9d85294ee4d952214117d7ece5f0ef9
RickeyAuger <augerrickey@gmail.com>
2025-03-11 21:47:34 -0400

    changed rate of fire

--- player.py
+++ player.py
@@ -5,11 +5,12 @@ from constants import PLAYER_RADIUS
 from constants import PLAYER_TURN_SPEED
 from constants import PLAYER_SPEED
 from constants import PLAYER_SHOOT_SPEED
+from constants import PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-       
+        self.timer = 0
         self.rotation = 0
 
     
@@ -45,14 +46,23 @@ class Player(CircleShape):
         if keys[pygame.K_SPACE]:
             self.shoot()
         
+        self.timer -= dt
+        if self.timer < 0:
+            self.timer = 0
+        
+        
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.timer > 0:
+            return
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0,1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+        self.timer = PLAYER_SHOOT_COOLDOWN
+       
 
 
 
