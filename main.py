e0cfd005589ff256c24e5079c13045c26aa22a69
RickeyAuger <augerrickey@gmail.com>
2025-03-11 22:42:17 -0400

    finished split mechanics. Asteroids complete

--- main.py
+++ main.py
@@ -58,7 +58,7 @@ def main():
         for asteroid in asteroids:
             for shot in shots:
                 if asteroid.check_for_collisions(shot) == True:
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
         
         screen.fill((0, 0 ,0))
